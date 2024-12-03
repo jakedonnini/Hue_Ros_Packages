df99c2c70a48b4178354ba88df155d7f84bd1f07
Jake Donnini <77365399+jakedonnini@users.noreply.github.com>
2024-12-03 15:42:29 -0500

    Update waypoint_node.py
    made points 3 times bigger

--- get_waypoints/get_waypoints/waypoint_node.py
+++ get_waypoints/get_waypoints/waypoint_node.py
@@ -39,8 +39,8 @@ class CoordinatesPublisher(Node):
         x, y, t = coord
 
         coord_msg = Coordinates()
-        coord_msg.x = x/1.5 # make smaller by 1.5
-        coord_msg.y = y/1.5
+        coord_msg.x = x*3 # make smaller by 1.5
+        coord_msg.y = y*3
         coord_msg.toggle = t
 
         # Publish the x and y coordinates
