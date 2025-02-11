090cacd532470583241d02dfd42f16467b2ba1dd
Jake Donnini <77365399+jakedonnini@users.noreply.github.com>
2025-02-11 12:43:10 -0500

    Update navigation_node.py
    added self.shouldBePainting to up to speed to avoid accidental trigger

--- navigator/navigator/navigation_node.py
+++ navigator/navigator/navigation_node.py
@@ -400,7 +400,7 @@ class GPSSubscriberPublisher(Node):
 
         # if less than 3/4 of nominal speed then stop painting
         # when speed is reached the next block of code should turn sprayer back on
-        notUpToSpeed = avgSpeed <= (pwmAvg * 0.75)
+        notUpToSpeed = avgSpeed <= (pwmAvg * 0.75) and self.shouldBePainting
         
         # only send the toggle comands once
         paintingIncorrect = int(self.shouldBePainting) != self.isPainting
