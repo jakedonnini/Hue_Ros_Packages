c0dc368f3e05972cc1937269f5d4f342dcda93b2
emmety345 <138820917+emmety345@users.noreply.github.com>
2025-02-20 22:33:04 -0500

    Update gps3_node.py
    changed calculations for lat and long; individual origins

--- gps_publisher/gps_publisher/gps3_node.py
+++ gps_publisher/gps_publisher/gps3_node.py
@@ -31,11 +31,15 @@ class GPSFusionNode(Node):
         self.gps1 = None
         self.gps2 = None
 
-        self.origin_lat = None
-        self.origin_lon = None
-
-        self.lat_to_cm = 111139.0 * 100 
-        self.lon_to_cm = 111139.0 * 100 * np.cos(np.radians(self.origin_lat or 0))
+        self.origin_lat_1 = None
+        self.origin_lon_1 = None
+        self.lat_to_cm_1 = 111139.0 * 100 
+        self.lon_to_cm_1 = 111139.0 * 100 * np.cos(np.radians(self.origin_lat_1 or 0))
+        
+        self.origin_lat_2 = None
+        self.origin_lon_2 = None
+        self.lat_to_cm_2 = 111139.0 * 100
+        self.lon_to_cm_2 = 11139.0 * 100 * np.cos(np.radians(self.origin_lat_2 or 0))
     
     def gps_callback_1(self, msg):
         self.gps1 = (msg.x, msg.y)
@@ -48,18 +52,44 @@ class GPSFusionNode(Node):
     def compute_and_publish(self):
         if self.gps1 is None or self.gps2 is None:
             return
+        
         lat1, lon1 = self.gps1
         lat2, lon2 = self.gps2
-        if not self.origin_lat or not self.origin_lon:
-            self.origin_lat = (lat1 + lat2) / 2.0
-            self.origin_lon = (lon1 + lon2) / 2.0
-            self.lon_to_cm = 111139.0 * 100 * np.cos(np.radians(self.origin_lat))
+        
+        if not self.origin_lat_1 and not self.origin_lon_1:
+            self.origin_lat_1 = lat1
+            self.origin_lon_1 = lon1
+            self.lon_to_cm_1 = 111139.0 * 100 * np.cos(np.radians(self.origin_lat_1)
+                                                       
+        if not self.origin_lat_2 and not self.origin_lon_2:
+            self.origin_lat_2 = lat2
+            self.origin_lon_2 = lon2
+            self.lon_to_cm_2 = 111139.0 * 100 * np.cos(np.radians(self.origin_lat_2))
             
         #lat1, lat2 = lat1*self.lat_to_cm * np.cos(np.radians(lat1 or 0)), self.lat_to_cm * np.cos(np.radians(lat2 or 0))*self.lat_to_cm
         #lon1, lon2 = lon1*self.lon_to_cm, lon2*self.lon_to_cm
-        lat1, lat2 = lat1 * self.lat_to_cm, lat2 * self.lat_to_cm
-        lon1, lon2 = lon1 * self.lon_to_cm, lon2 * self.lon_to_cm
+        #lat1, lat2 = lat1 * self.lat_to_cm, lat2 * self.lat_to_cm
+        #lon1, lon2 = lon1 * self.lon_to_cm, lon2 * self.lon_to_cm
+        x1, y1 = lat1 * self.lat_to_cm_1 - self.origin_lat_1, lon1 * self.lon_to_cm_1 - self.origin_lon_1
+        x2, y2 = lat2 * self.lat_to_cm_2 - self.origin_lat_2, lon2 * self.lon_to_cm_2 - self.origin_lon_2
+
+        mid_x = (x1 + x2) / 2.0
+        mid_y = (y1 + y2) / 2.0
+
+        delta_x = x2 - x1
+        delta_y = y2 - y1
+        angle = math.radians(math.atan2(delta_x, delta_y))
         
+        # Publish midpoint and angle
+        gps_data = GpsData()
+        gps_data.x = mid_x
+        gps_data.y = mid_y
+        gps_data.angle = angle
+        self.gps_fusion_publisher.publish(gps_data)
+        
+        self.get_logger().info(f'Midpoint: ({mid_x}, {mid_y}), Angle: {angle} degrees')
+        
+        '''
         # Compute the midpoint
         mid_lat = (lat1 + lat2) / 2.0
         mid_lon = (lon1 + lon2) / 2.0
@@ -77,6 +107,7 @@ class GPSFusionNode(Node):
         self.gps_fusion_publisher.publish(gps_data)
         
         self.get_logger().info(f'Midpoint: ({mid_lat}, {mid_lon}), Angle: {angle} degrees')
+        '''
         
 
 def main(args=None):
