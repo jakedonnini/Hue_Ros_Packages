add coord fixes. rotation and offest should be good now

gps_publisher/gps_publisher/gps3_node.py
@@ -45,16 +45,21 @@ class GPSFusionNode(Node):
         mid_x = (x1 + x2) / 2.0
         mid_y = (y1 + y2) / 2.0
 
-        # if self.x_mid_zero == 0 and self.y_mid_zero == 0:
-        #     self.x_mid_zero = mid_x
-        #     self.y_mid_zero = mid_y
+        if self.x_mid_zero == 0 and self.y_mid_zero == 0:
+            self.x_mid_zero = mid_x
+            self.y_mid_zero = mid_y
 
-        # mid_x -= self.x_mid_zero
-        # mid_y -= self.y_mid_zero
-
-        delta_x = x2 - x1
-        delta_y = y2 - y1
+        delta_x = x1 - x2
+        delta_y = y1 - y2
         angle = math.atan2(delta_y, delta_x) + np.pi
+
+        # Adjust coordinates to origin
+        mid_x -= self.x_mid_zero
+        mid_y -= self.y_mid_zero
+        x1 -= self.x_mid_zero
+        x2 -= self.x_mid_zero
+        y1 -= self.y_mid_zero
+        y2 -= self.y_mid_zero
         
         gps_data = GpsData()
         gps_data.x = mid_x

navigator/navigator/kalman.py
@@ -161,8 +161,17 @@ class KalmanFilter(Node):
         # emmet is confused :(((((
         if not self.rotation_calculated and self.gps_Theta != 0:
             self.rotation_calculated = True
-            self.rotThata = self.gps_Theta
-            self.Rot = np.array([[-np.cos(self.rotThata), -np.sin(self.rotThata)], [np.sin(self.rotThata), np.cos(self.rotThata)]]) # this includes rotation and relfection about y-axis
+
+            # make sure the roation is all the way
+            # this worked for most cases in testing
+            offest_angle = 0
+            if self.gps_Theta > np.pi:
+                offest_angle = 0
+            else:
+                offest_angle = np.pi
+
+            self.rotThata = offest_angle - self.gps_Theta
+            self.Rot = np.array([[-np.cos(self.rotThata), np.sin(self.rotThata)], [np.sin(self.rotThata), np.cos(self.rotThata)]]) # this includes rotation and relfection about y-axis
 
         # apply the R to the points
         self.DR_x_rot = (self.Rot[0, 0] * self.DR_x + self.Rot[0, 1] * self.DR_y)
@@ -188,7 +197,6 @@ class KalmanFilter(Node):
         # self.get_logger().info(f"State before rot: {state}")
 
         state = Rot_Extended @ state
-        # state[2, 0] = self.DR_angle_rot
 
         # self.get_logger().info(f"State after rot: {state}")
 