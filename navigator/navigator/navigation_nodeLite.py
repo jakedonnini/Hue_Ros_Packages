40fe3a8cc5440170f43c2baba59050eb6ea32c26
Jake <jdonnini@seas.upenn.edu>
2025-03-01 13:18:58 -0500

    nav change dir error

--- navigator/navigator/navigation_nodeLite.py
+++ navigator/navigator/navigation_nodeLite.py
@@ -181,8 +181,6 @@ class GPSSubscriberPublisher(Node):
         """Adjust and publish PWMR and PWML values based on GPS data."""
         dist, thetaError, distToLine = self.getPosError()
 
-        thetaError = thetaError*self.dir # invert the direction TODO: check if this is correct
-
         # KQ = 20*2  # turn speed
         # pwmDel = KQ * thetaError
         pwmAvg = 20 # normally 60
