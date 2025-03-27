d6dc93f4f4fe7f26406d79f50a1042a16b7dcf77
Jake <jdonnini@seas.upenn.edu>
2025-03-27 19:34:02 -0400

    fix error in kalman

--- navigator/navigator/kalman.py
+++ navigator/navigator/kalman.py
@@ -153,7 +153,7 @@ class KalmanFilter(Node):
                 # Debugging Log
                 self.get_logger().info(
                     f'[GPS]: {round(self.gps_x, 2)}, {round(self.gps_y, 2)}, {round(self.gps_Theta, 2)} '
-                    f'[ENCODER] V: {round(self.V, 2)} dV: {round(self.dV, 2)} before {round(self.stateBeforeRot[0, 0], 2)}, {round(self.stateBeforeRot[1, 0], 2)}, {round(self.stateBeforeRot[2, 0], 2)} After {round(self.stateAfterRot[0, 0], 2)}, {round(self.stateAfterRot[1, 0], 2)}, {round(self.stateAfterRot[2, 0], 2)}' # add states
+                    f'[ENCODER] V: {round(self.V, 2)} dV: {round(self.dV, 2)} before {round(self.stateBeforeRot[0, 0], 2)}, {round(self.stateBeforeRot[1, 0], 2)}, {round(self.stateBeforeRot[2, 0], 2)} After {round(self.stateAfterRot[0, 0], 2)}, {round(self.stateAfterRot[1, 0], 2)}, {round(self.stateAfterRot[2, 0], 2)}'
                     f'[KALMAN] X: {round(self.x[0, 0], 2)} Y: {round(self.x[1, 0], 2)} Theta: {round(self.x[2, 0], 2)}'
                 )
 
