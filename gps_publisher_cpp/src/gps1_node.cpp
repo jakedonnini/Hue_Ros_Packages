a754960f9ff551baa4ff53c139a4b8e4cfe698d8
Ethan Ma <ethma727@gmail.com>
2025-04-03 01:30:36 -0400

    apfijapwoeijf

--- gps_publisher_cpp/src/gps1_node.cpp
+++ gps_publisher_cpp/src/gps1_node.cpp
@@ -196,7 +196,7 @@ public:
 
     int main(int argc, char **argv) {
     rclcpp::init(argc, argv);
-    auto node = std::make_shared<GPSPublisher>();
+    auto node = std::make_shared<GPSPublisher1>();
     rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
