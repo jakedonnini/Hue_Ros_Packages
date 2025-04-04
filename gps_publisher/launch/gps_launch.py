82197ab9c2108a8aadf95564ef90c96e1aac4216
Ethan Ma <ethma727@gmail.com>
2025-04-03 23:59:39 -0400

    yea idk

--- gps_publisher/launch/gps_launch.py
+++ gps_publisher/launch/gps_launch.py
@@ -1,23 +1,45 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+# def generate_launch_description():
+#     return LaunchDescription([
+#         Node(
+#             package='gps_publisher',
+#             executable='gps_node',
+#             name='gps_publisher',
+#             output='screen'
+#         ),
+#         Node(
+#             package='gps_publisher',
+#             executable='gps2_node',
+#             name='gps2_publisher',
+#             output='screen'
+#         ),
+#         Node(
+#             package='gps_publisher',
+#             executable='gps3_node',
+#             name='gps_fusion_node',
+#             output='screen'
+#         )
+#     ])
+
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            package='gps_publisher',
-            executable='gps_node',
-            name='gps_publisher',
+            package='gps_publisher_cpp',
+            executable='gps_pub1',
+            name='gps1_publisher',
             output='screen'
         ),
         Node(
-            package='gps_publisher',
-            executable='gps2_node',
+            package='gps_publisher_cpp',
+            executable='gps_pub2',
             name='gps2_publisher',
             output='screen'
         ),
         Node(
-            package='gps_publisher',
-            executable='gps3_node',
+            package='gps_publisher_cpp',
+            executable='gps_fus',
             name='gps_fusion_node',
             output='screen'
         )
